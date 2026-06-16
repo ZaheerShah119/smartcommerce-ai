30a7d364fbeed91e0dd8f7a3c336fe0236d65b1d
Zaheer <syedzaheer2026@gmail.com>
2026-06-17 00:19:12 +0500

    fixed base url variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Creating a global optimized Axios instance for backend routing
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
